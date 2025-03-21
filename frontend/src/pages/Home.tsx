2a548bb218e355a7242f047eb4ab9d4c677cd8c9
Tanzida Nowshin <nowshinsara555@gmail.com>
2025-03-22 03:03:48 +0600

    slider Added

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,30 @@
+import { useState, useEffect } from "react";
 import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 
 const Home = () => {
+  const images = [
+    "https://images.pexels.com/photos/1193747/pexels-photo-1193747.jpeg", // Image 1
+    "https://images.pexels.com/photos/1561947/pexels-photo-1561947.jpeg", // Image 2
+    "https://images.pexels.com/photos/3787596/pexels-photo-3787596.jpeg", // Image 3
+  ];
+
+  const [currentImage, setCurrentImage] = useState(0);
+
+  const nextImage = () => {
+    setCurrentImage((prevImage) => (prevImage + 1) % images.length);
+  };
+
+  const prevImage = () => {
+    setCurrentImage((prevImage) =>
+      prevImage === 0 ? images.length - 1 : prevImage - 1
+    );
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(nextImage, 5000);
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-gray-900 text-white px-6">
       <div className="max-w-3xl text-center">
@@ -12,6 +36,28 @@ const Home = () => {
           <span className="text-blue-300">Software Engineer</span>, crafting
           beautiful and efficient digital experiences.
         </p>
+
+        {/* Slideshow */}
+        <div className="relative mb-6">
+          <img
+            src={images[currentImage]}
+            alt="Slideshow Image"
+            className="w-64 h-64 sm:w-80 sm:h-80 object-cover rounded-lg shadow-lg"
+          />
+          <button
+            onClick={prevImage}
+            className="absolute top-1/2 left-2 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full"
+          >
+            &#60;
+          </button>
+          <button
+            onClick={nextImage}
+            className="absolute top-1/2 right-2 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full"
+          >
+            &#62;
+          </button>
+        </div>
+
         <div className="flex gap-4 justify-center">
           <a
             href="https://github.com/your-github"
