0ccc4deb8d843da8c1f008c6a30e87d8645b9cda
Tanzida Nowshin <nowshinsara555@gmail.com>
2025-03-22 02:19:01 +0600

    react bug solved

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+
 import Projects from "./pages/Projects";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
