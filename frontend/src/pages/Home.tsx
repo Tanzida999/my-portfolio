e12837efaf4c95f04e23f447244d7f64d8fa2598
Tanzida Nowshin <nowshinsara555@gmail.com>
2025-03-22 22:44:27 +0600

    Updated

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,24 @@
 import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
-import banner from "../../src/images/banner.png";
+import banner3 from "../../src/images/banner3.png";
+import banner2 from "../../src/images/banner2.png";
 
 const Home = () => {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-900 text-white px-6">
+    <div
+      className="relative overflow-clip min-h-screen
+    text-white bg-[linear-gradient(to_bottom,#000,+071E18_35%,#208A65_67%,#35FB8E_85%)]"
+    >
+      {/* Banner Image */}
+      <div className=" absolute bg-black w-[2400px] h-[1000px] rounded-[50%] left-1/2 -translate-x-1/2 bg-[radial-gradient(closest-side,#000_85%,#249974)] top-[450px] border-[1px] border-[#8CD6DE]/30container ml-40">
+        <div className="container w-[1200px] h-[500px] ml-20">
+          <img
+            src={banner3}
+            alt="Banner Image"
+            className="w-full h-full object-cover rounded-lg shadow-lg p-40"
+          />
+        </div>
+      </div>
+
       <div className="max-w-3xl text-center">
         <h1 className="text-5xl font-bold mb-4 animate-fade-in">
           Hi, I'm <span className="text-blue-400">Tanzida Nowshin</span>
@@ -13,19 +28,6 @@ const Home = () => {
           <span className="text-blue-300">Software Engineer</span>, crafting
           beautiful and efficient digital experiences.
         </p>
-
-        <div className="container ">
-          <img src={banner} className="w-[400px] h-[300px]" alt="" />
-        </div>
-
-        {/* Banner Image */}
-        {/* <div className="container ml-40">
-          <img
-            src={banner}
-            alt="Banner Image"
-            className="w-[400px] h-[200px] rounded-lg shadow-lg"
-          />
-        </div> */}
 
         <div className="flex gap-4 justify-center">
           <a
