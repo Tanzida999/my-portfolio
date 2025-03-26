526a51626c33240305f2da9583185fd0e6bdbae3
Tanzida Nowshin <nowshinsara555@gmail.com>
2025-03-27 01:03:23 +0600

    Updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import Header from "./components/Header";
 import Projects from "./pages/Projects";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
+import Stack from "./pages/Stack";
 
 import "./index.css";
-import Stack from "./pages/Stack";
 
 const App = () => {
   return (
@@ -16,19 +21,27 @@ const App = () => {
         <Header />
         <main className="container mx-auto py-10 px-6">
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<HomePage />} />
             <Route path="/projects" element={<Projects />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
-            {/* <Route path="/stack" element={<Stack />} */}
+            <Route path="/stack" element={<Stack />} />
           </Routes>
-          <About />
-          <Projects />
-          {/* <Stack /> */}
         </main>
         <Footer />
       </Router>
     </div>
+  );
+};
+
+const HomePage = () => {
+  return (
+    <>
+      <Home />
+      <About />
+      <Projects />
+      <Stack />
+    </>
   );
 };
 
