2fdc171fff4f5f34f1806175c9d0259a08ec3d36
Tanzida Nowshin <nowshinsara555@gmail.com>
2025-03-29 00:02:54 +0600

    Stack bug fixed

--- frontend/src/pages/Stack.jsx
+++ frontend/src/pages/Stack.jsx
@@ -1,5 +1,60 @@
+import { motion, useAnimation, useInView } from "framer-motion";
+import React, { useEffect } from "react";
+import { SiFramer, SiFigma, SiReact, SiNodedotjs } from "react-icons/si";
+
+const stackItems = [
+  {
+    id: 1,
+    name: "Framer",
+    icon: <SiFramer size={100} />,
+    color: "text-purple-400",
+  },
+  {
+    id: 2,
+    name: "Figma",
+    icon: <SiFigma size={100} />,
+    color: "text-pink-400",
+  },
+  {
+    id: 3,
+    name: "React",
+    icon: <SiReact size={100} />,
+    color: "text-blue-400",
+  },
+  {
+    id: 4,
+    name: "Node.js",
+    icon: <SiNodedotjs size={100} />,
+    color: "text-green-400",
+  },
+];
 const Stack = () => {
-  return <div>Stack</div>;
+  const controls = useAnimation();
+  const ref = React.useRef(null);
+  const inView = useInView(ref, { threshold: 0.1 });
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    } else {
+      controls.start("hidden");
+    }
+  }, [controls, inView]);
+  return (
+    <div>
+      <section
+        className="py-24 md:py-24 max-w-[1200px] mx-auto text-center"
+        id="stack"
+      >
+        <h2 className="text-7xl text-gray-100 font-bold mb-20">My Stack</h2>
+        <div className="flex flex-wrap justify-center gap-8" ref={ref}>
+          {stackItems.map((item, index) => (
+            <motion.div key={item.id}></motion.div>
+          ))}
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Stack;
